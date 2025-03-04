06a8303def29dce68e0977b9ddba42c0f489ec96
PixeledCode <ashoaib540918@gmail.com>
2025-03-04 12:36:47 +0530

    add work page in progress

---
+++ app/work/page.tsx
@@ -0,0 +1,7 @@
+export default function Work() {
+  return (
+    <main className="flex items-center justify-center h-[calc(100lvh_-_230px)]">
+      <span className="text-4xl">Page in progress...</span>
+    </main>
+  );
+}
